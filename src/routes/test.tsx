52cb0302b3330504964b8c81cd17a088b0518c8a
jesseb34r <jessea.login@gmail.com>
2023-04-12 21:54:47 -0700

    feat: add test page

---
+++ src/routes/test.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+  return (
+    <main class="flex select-none flex-col items-center">
+      <h1 class="mb-2.5 text-6xl font-semibold tracking-wide">Test Page</h1>
+      <p class="text-lg text-sand11">Make features work</p>
+    </main>
+  );
+}
