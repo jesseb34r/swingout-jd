da7a7949cdfd440c35d3f45879b812cccc886991
jesseb34r <jessea.login@gmail.com>
2023-04-12 21:54:38 -0700

    fix: use sand instead of mauve color

--- src/routes/(home).tsx
+++ src/routes/(home).tsx
@@ -2,7 +2,7 @@ export default function HomePage() {
   return (
     <main class="flex flex-col items-center">
       <h1 class="mb-2.5 text-6xl font-semibold tracking-wide">SwingOut</h1>
-      <p class="text-mauve11 text-lg">
+      <p class="text-lg text-sand11">
         The easiest way to play magic on the web
       </p>
     </main>
--- src/routes/deck.tsx
+++ src/routes/deck.tsx
@@ -2,7 +2,7 @@ export default function DeckPage() {
   return (
     <main class="flex flex-col items-center">
       <h1 class="mb-2.5 text-6xl font-semibold tracking-wide">Deck Page</h1>
-      <p class="text-lg text-mauve11">View your deck before you play with it</p>
+      <p class="text-lg text-sand11">View your deck before you play with it</p>
     </main>
   );
 }
