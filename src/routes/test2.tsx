3716c235233d671181f113a3d537a7bf832f3396
jesseb34r <jessea.login@gmail.com>
2023-04-17 19:59:21 -0700

    use object of elements instead of array

--- src/routes/test2.tsx
+++ src/routes/test2.tsx
@@ -1,193 +1,155 @@
 import {
-  Accessor,
   For,
-  Setter,
   Show,
   VoidComponent,
   createContext,
   createSignal,
   createUniqueId,
   useContext,
-  ComponentProps,
 } from "solid-js";
 import { createStore, produce } from "solid-js/store";
 import { JSX } from "solid-js/web/types/jsx";
 
-const DraggableBox: VoidComponent<{
+type Position = { x: number; y: number };
+
+type DraggableState = {
   id: string;
-  setRef: Setter<HTMLDivElement>;
-}> = (props) => {
-  const test2Context = useTest2Context();
-  const position = () =>
-    test2Context.state.elements.find((element) => element.id === props.id)!
-      .position;
+  position: Position;
+  dragStartPosition: Position;
+};
+
+type DragState = {
+  elements: Record<string, DraggableState>;
+  selectedElementIds: string[];
+  dragStartMousePosition: Position;
+};
+
+type DragContextValue = {
+  state: DragState;
+  addElement: (toAdd: DraggableState) => void;
+  moveElement: (toMoveId: string, toPosition: Position) => void;
+  setDragStartPositions: () => void;
+  setDragStartMousePosition: (position: Position) => void;
+  selectElement: (toSelectId: string) => void;
+  unSelectElement: (toUnSelectId: string) => void;
+  handleDrag: (e: MouseEvent) => void;
+};
+
+const DragContext = createContext<DragContextValue>();
+
+const useDragContext = () => {
+  const context = useContext(DragContext);
+  if (!context) {
+    throw new Error(
+      "[drag-context]: useDragContext must be used within a DragContext provider"
+    );
+  }
+  return context;
+};
+
+const Draggable: VoidComponent<{ id: string }> = (props) => {
+  const dragContext = useDragContext();
 
   const handleMouseUp = () => {
-    test2Context.clearSelection();
-    document.removeEventListener("mousemove", test2Context.handleDrag);
+    document.removeEventListener("mousemove", dragContext.handleDrag);
     document.removeEventListener("mouseup", handleMouseUp);
   };
 
   const handleMouseDown: JSX.EventHandler<HTMLDivElement, MouseEvent> = (e) => {
     e.preventDefault();
-    test2Context.addSelectedElement(props.id);
-    test2Context.setDragStartMousePosition({ x: e.clientX, y: e.clientY });
-    test2Context.setDragStartPositions();
-    console.log("set new start positions");
+    if (!dragContext.state.selectedElementIds.includes(props.id)) {
+      dragContext.selectElement(props.id);
+    } else {
+      dragContext.unSelectElement(props.id);
+    }
 
-    document.addEventListener("mousemove", test2Context.handleDrag);
+    dragContext.setDragStartMousePosition({ x: e.clientX, y: e.clientY });
+    dragContext.setDragStartPositions();
+
+    document.addEventListener("mousemove", dragContext.handleDrag);
     document.addEventListener("mouseup", handleMouseUp);
   };
 
   return (
     <div
       style={{
-        top: `${position().y}px`,
-        left: `${position().x}px`,
+        top: `${dragContext.state.elements[props.id].position.y}px`,
+        left: `${dragContext.state.elements[props.id].position.x}px`,
       }}
-      ref={props.setRef}
-      class="fixed h-40 w-40 border-orange6 bg-blue3"
+      classList={{
+        "fixed h-40 w-40 rounded bg-blue3": true,
+        "border-4 border-orange6":
+          !!dragContext.state.selectedElementIds.includes(props.id),
+      }}
       onMouseDown={handleMouseDown}
-    >
-      {JSON.stringify(
-        test2Context.state.elements.find((element) => element.id === props.id)!
-          .dragStartPosition
-      )}
-    </div>
+    />
   );
 };
 
-type DragabbleBoxState = {
-  id: string;
-  ref: Accessor<HTMLDivElement | undefined>;
-  position: { x: number; y: number };
-  dragStartPosition: { x: number; y: number };
-};
-
-type Test2State = {
-  elements: DragabbleBoxState[];
-  selectedElementIds: string[];
-  dragStartMousePosition: { x: number; y: number };
-};
-
-type Test2ContextValue = {
-  state: Test2State;
-  moveElement: (id: string, toPosition: { x: number; y: number }) => void;
-  addElement: (toAdd: DragabbleBoxState) => void;
-  addSelectedElement: (id: string) => void;
-  clearSelection: () => void;
-  setDragStartPositions: () => void;
-  setDragStartMousePosition: (position: { x: number; y: number }) => void;
-  handleDrag: (e: MouseEvent) => void;
-};
-
-const Test2Context = createContext<Test2ContextValue>();
-
-const useTest2Context = () => {
-  const context = useContext(Test2Context);
-
-  if (context === undefined) {
-    throw new Error(
-      "[my-app]: `useTest2Context` must be used within Test2Context scope"
-    );
-  }
-
-  return context;
-};
-
 export default function Test2Page() {
-  const [state, setState] = createStore<Test2State>({
-    elements: [],
+  const [state, setState] = createStore<DragState>({
+    elements: {},
     selectedElementIds: [],
     dragStartMousePosition: { x: 0, y: 0 },
   });
-  const context: Test2ContextValue = {
-    state,
-    moveElement(id, toPosition) {
-      if (!state.elements.find((element) => element.id === id)) {
-        throw new Error("element id not found");
-      }
 
-      setState(
-        "elements",
-        (element) => element.id === id,
-        "position",
-        toPosition
-      );
+  const context: DragContextValue = {
+    state,
+    addElement: (toAdd) => {
+      setState("elements", toAdd.id, toAdd);
     },
-    addElement(toAdd) {
-      setState(produce((state) => state.elements.push(toAdd)));
+    moveElement: (toMoveId, toPosition) => {
+      setState("elements", toMoveId, "position", toPosition);
     },
-    addSelectedElement(id) {
-      if (!state.elements.find((element) => element.id === id)) {
-        throw new Error("element id not found");
-      }
-      // console.log("adding id " + id);
-      setState(produce((state) => state.selectedElementIds.push(id)));
-      // console.log("current selection is " + state.selectedElementIds);
-    },
-    clearSelection() {
-      setState("selectedElementIds", []);
-      // console.log("selection cleared");
-    },
-    setDragStartMousePosition(position) {
-      setState("dragStartMousePosition", position);
-      console.log(
-        "starting mouse position: " +
-          JSON.stringify(state.dragStartMousePosition)
-      );
-    },
-    setDragStartPositions() {
+    setDragStartPositions: () => {
       state.selectedElementIds.forEach((id) => {
-        console.log(
-          "starting element position: " +
-            id +
-            ": " +
-            JSON.stringify(
-              state.elements.find((element) => element.id === id)!.position
-            )
-        );
         setState(
           "elements",
-          (element) => element.id === id,
-          (prev) => {
-            prev.dragStartPosition = prev.position;
-            return prev;
-          }
+          id,
+          "dragStartPosition",
+          state.elements[id].position
         );
       });
     },
-    handleDrag(e) {
-      state.selectedElementIds.forEach((id) => {
-        // console.log(
-        //   JSON.stringify(
-        //     state.elements.find((element) => element.id === id)!
-        //       .dragStartPosition
-        //   )
-        // );
-        // console.log(
-        //   JSON.stringify(
-        //     state.elements.find((element) => element.id === id)!.position
-        //   )
-        // );
-        // console.log(JSON.stringify(state.elements));
-        setState(
-          "elements",
-          (element) => element.id === id,
-          "position",
-          () => {
-            return {
-              x:
-                state.elements.find((element) => element.id === id)!
-                  .dragStartPosition.x +
-                (e.clientX - state.dragStartMousePosition.x),
-              y:
-                state.elements.find((element) => element.id === id)!
-                  .dragStartPosition.y +
-                (e.clientY - state.dragStartMousePosition.y),
-            };
-          }
+    setDragStartMousePosition: (position) => {
+      setState("dragStartMousePosition", position);
+    },
+    selectElement: (toSelect) => {
+      if (state.selectedElementIds.includes(toSelect)) {
+        throw new Error(
+          "[drag-context]: cannot select an element that is already selected"
         );
+      }
+      setState(
+        produce((currentState) =>
+          currentState.selectedElementIds.push(toSelect)
+        )
+      );
+    },
+    unSelectElement: (toUnselect) => {
+      if (!state.selectedElementIds.includes(toUnselect)) {
+        throw new Error(
+          "[drag-context]: cannot unselect an element that is not selected"
+        );
+      }
+      setState(
+        produce(
+          (currentState) =>
+            (currentState.selectedElementIds =
+              currentState.selectedElementIds.filter((id) => id !== toUnselect))
+        )
+      );
+    },
+    handleDrag: (e) => {
+      state.selectedElementIds.forEach((id) => {
+        setState("elements", id, "position", {
+          x:
+            state.elements[id].dragStartPosition.x +
+            (e.clientX - state.dragStartMousePosition.x),
+          y:
+            state.elements[id].dragStartPosition.y +
+            (e.clientY - state.dragStartMousePosition.y),
+        });
       });
     },
   };
@@ -261,8 +223,8 @@ export default function Test2Page() {
     return Math.abs(activePosition().x - initialPosition().x);
   };
 
-  const makeElements = (numToMake: number) => {
-    const toReturn: ComponentProps<typeof DraggableBox>[] = [];
+  const makeElements = (numToMake: number): string[] => {
+    const toReturn: string[] = [];
 
     for (let i = 1; i <= numToMake; i++) {
       const [ref, setRef] = createSignal<HTMLDivElement>();
@@ -274,23 +236,22 @@ export default function Test2Page() {
 
       context.addElement({
         id,
-        ref: ref,
         position: initialPosition,
         dragStartPosition: initialPosition,
       });
-      toReturn.push({ id, setRef });
+      toReturn.push(id);
     }
 
     return toReturn;
   };
 
-  const elements = makeElements(2);
+  const startingElementIds = makeElements(2);
 
   return (
     <main class="flex select-none flex-col text-center">
       <h1 class="mb-2.5 text-6xl font-semibold tracking-wide">Test Page</h1>
       <p class="text-lg text-sand11">Make features work</p>
-      <Test2Context.Provider value={context}>
+      <DragContext.Provider value={context}>
         <div
           ref={setZoneRef}
           onMouseDown={handleMouseDown}
@@ -308,13 +269,13 @@ export default function Test2Page() {
               class="fixed z-50 overflow-clip border bg-sand11/10"
             ></div>
           </Show>
-          <For each={elements}>
-            {(element) => {
-              return <DraggableBox {...element} />;
+          <For each={startingElementIds}>
+            {(id) => {
+              return <Draggable id={id} />;
             }}
           </For>
         </div>
-      </Test2Context.Provider>
+      </DragContext.Provider>
     </main>
   );
 }
